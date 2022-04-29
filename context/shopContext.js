03bdb4df44f09da1157e2d37811a43fc4eef841f
Ted <p0599847@protonmail.com>
2022-04-29 16:18:22 +0200

    persisting data

--- context/shopContext.js
+++ context/shopContext.js
@@ -1,59 +1,81 @@
-import { createContext, useState, useEffect } from 'react'
-import { createCheckout, updateCheckout } from '../lib/shopify'
+import { createContext, useState, useEffect } from "react";
+import { createCheckout, updateCheckout } from "../lib/shopify";
 
-const CartContext = createContext()
+const CartContext = createContext();
 
 export default function ShopProvider({ children }) {
-  const [cart, setCart] = useState([])
-  const [cartOpen, setCartOpen] = useState(false)
-  const [checkoutId, setCheckoutId] = useState('')
-  const [checkoutUrl, setCheckoutUrl] = useState('')
+  const [cart, setCart] = useState([]);
+  const [cartOpen, setCartOpen] = useState(false);
+  const [checkoutId, setCheckoutId] = useState("");
+  const [checkoutUrl, setCheckoutUrl] = useState("");
 
+  // get if we already have a card in local storage
+  useEffect(() => {
+    if (localStorage.checkout_id) {
+      const cardObject = JSON.parse(localStorage.checkout_id);
+
+      if (cardObject[0].id) {
+        setCart([cardObject[0]]);
+      } else if (cardObject[0].length > 0) {
+        setCart(...[cardObject[0]]);
+      }
+
+      setCheckoutId(cardObject[1].id);
+      setCheckoutUrl(cardObject[1].webUrl);
+    }
+  }, []);
 
   async function addToCart(newItem) {
-    if(cart.length === 0) {
-      setCart([newItem])
+    if (cart.length === 0) {
+      setCart([newItem]);
 
-      console.log(newItem)
+      console.log(newItem);
 
-      const checkout = await createCheckout(newItem.id, newItem.variantQuantity)
+      const checkout = await createCheckout(
+        newItem.id,
+        newItem.variantQuantity
+      );
 
-      setCheckoutId(checkout.id)
-      setCheckoutUrl(checkout.webUrl)
+      setCheckoutId(checkout.id);
+      setCheckoutUrl(checkout.webUrl);
 
-      localStorage.setItem("checkout_id", JSON.stringify([newItem, checkout]))
+      localStorage.setItem("checkout_id", JSON.stringify([newItem, checkout]));
     } else {
-      let newCart = [...cart]
+      let newCart = [...cart];
 
-      cart.map(item => {
+      cart.map((item) => {
         if (item.id === newItem.id) {
-          item.variantQuantity++
-          newCart = [...cart]
+          item.variantQuantity++;
+          newCart = [...cart];
         } else {
-          newCart = [...cart, newItem]
+          newCart = [...cart, newItem];
         }
-      })
+      });
 
-      setCart(newCart)
-      const newCheckout = await updateCheckout(checkoutId, newCart)
-      localStorage.setItem("checkout_id", JSON.stringify([newCart, newCheckout]))
+      setCart(newCart);
+      const newCheckout = await updateCheckout(checkoutId, newCart);
+      localStorage.setItem(
+        "checkout_id",
+        JSON.stringify([newCart, newCheckout])
+      );
     }
   }
 
-
   return (
-    <CartContext.Provider value={{ 
-      cart,
-      cartOpen,
-      setCartOpen,
-      addToCart,
-      checkoutUrl
-    }}>
+    <CartContext.Provider
+      value={{
+        cart,
+        cartOpen,
+        setCartOpen,
+        addToCart,
+        checkoutUrl,
+      }}
+    >
       {children}
     </CartContext.Provider>
-  )
+  );
 }
 
-const ShopConsumer = CartContext.Consumer
+const ShopConsumer = CartContext.Consumer;
 
-export { ShopConsumer, CartContext }
+export { ShopConsumer, CartContext };
