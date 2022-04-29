2af817f3f01aa55783c0f90093f29bf5f95c235f
Ted <p0599847@protonmail.com>
2022-04-29 22:11:45 +0200

    simple footer

---
+++ components/Footer.js
@@ -0,0 +1,39 @@
+const navigation = [
+  {
+    name: "A propos",
+    href: "#",
+  },
+  {
+    name: "Shop",
+    href: "#",
+  },
+  {
+    name: "Jobs",
+    href: "#",
+  },
+  {
+    name: "Conditions d'utilisation",
+    href: "#",
+  },
+];
+
+export default function Footer() {
+  return (
+    <footer className="bg-white">
+      <div className="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
+        <nav className="flex flex-wrap justify-center">
+          {navigation.map((item, i) => (
+            <div key={i} className="px-6 py-2">
+              <a href={item.href} className="text-gray-500 hover:text-gray-900">
+                {item.name}
+              </a>
+            </div>
+          ))}
+        </nav>
+        <p className="mt-8 text-center text-gray-400">
+          &copy; 2022 Ted's Learning Next Js & Shopify
+        </p>
+      </div>
+    </footer>
+  );
+}
--- components/Layout.js
+++ components/Layout.js
@@ -1,16 +1,13 @@
-import React from 'react'
-import Nav from './Nav'
+import React from "react";
+import Footer from "./Footer";
+import Nav from "./Nav";
 
 export default function Layout({ children }) {
   return (
     <div className="flex flex-col justify-between min-h-screen">
       <Nav />
-      <main>
-        {children}
-      </main>
-      <footer>
-        Footer
-      </footer>
+      <main>{children}</main>
+      <Footer />
     </div>
-  )
+  );
 }
