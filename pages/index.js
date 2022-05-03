9e05a0e7457d5ac19243129634bcf122e5ca3dd8
Ted <p0599847@protonmail.com>
2022-05-03 16:08:02 +0200

    add basic seo

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,42 @@
-import { getProductsInCollection } from "../lib/shopify"
-import ProductList from "../components/ProductList"
-
+import { getProductsInCollection } from "../lib/shopify";
+import ProductList from "../components/ProductList";
+import Hero from "../components/Hero";
+import Head from "next/head";
 
 export default function Home({ products }) {
-
   return (
-    <div className="text-3xl">
+    <div>
+      <Head>
+        <title>Shopinext</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta
+          http-equiv="Content-Type"
+          content="text/html; charset=ISO-8859-1"
+        />
+        <meta
+          name="description"
+          content="Plateforme e-commerce réalisé avec next.js et shopify"
+        />
+        <meta property="og:title" content="Shopinext" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://shopinext-ruby.vercel.app/" />
+        <meta
+          property="og:description"
+          content="Plateforme e-commerce réalisé avec next.js et shopify"
+        />
+        <meta property="og:locale" content="fr_FR" />
+        <meta property="og:site_name" content="Shopinext" />
+      </Head>
+      <Hero />
       <ProductList products={products} />
     </div>
-  )
+  );
 }
 
 export async function getStaticProps() {
-  const products = await getProductsInCollection()
+  const products = await getProductsInCollection();
 
   return {
     props: { products }, // will be passed to the page component as props
-  }
+  };
 }
